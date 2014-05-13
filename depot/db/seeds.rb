56471b1c33cea783a3c718dd33963f976da43710
yellowstar <845059331@qq.com>
2014-05-13 23:43:49 +0800

    set db seeds

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+Product.create!(
+  title: 'Programming Ruby 1.9 & 2.0',
+  description:
+  %{<p>
+      Ruby is the fastest growing and most exciting dynamic language
+      out there. If you need to get working programs delivered fast,
+      you should add Ruby to your toolbox.
+    </p>},
+  image_url: 'ruby.jpg',
+  price: 49.95)
+
