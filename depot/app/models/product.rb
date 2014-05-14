284747c37a06e6f44d267f7e939b7e211595c8a2
yellowstar <845059331@qq.com>
2014-05-14 22:51:49 +0800

    fix fred.gif should be valid

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with:    %r{\.(.gif|jpg|png)\Z}i,
+    with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be aURL for GIF, JPG or PNG image.'
   }
 end
