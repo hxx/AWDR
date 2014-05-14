2b246608835a52a7bfbfba226e17f17b82f2a3df
yellowstar <845059331@qq.com>
2014-05-14 23:17:07 +0800

    test product is not valid without a unique title - i18n by using fixture data

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -56,4 +56,13 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy",
+                          price:       1,
+                          image_url:   "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
