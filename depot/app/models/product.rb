b63e65e7159218a6ae3678d91de8d6aef650e977
yellowstar <845059331@qq.com>
2014-05-16 21:27:11 +0800

    product has_many line_items

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
