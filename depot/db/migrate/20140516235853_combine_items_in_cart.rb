002612fcbacce36486bae5278f233427f79bd3ff
yellowstar <845059331@qq.com>
2014-05-17 08:34:28 +0800

    replace the down method to split items with quantity>1 into multiple items

--- depot/db/migrate/20140516235853_combine_items_in_cart.rb
+++ depot/db/migrate/20140516235853_combine_items_in_cart.rb
@@ -18,4 +18,17 @@ class CombineItemsInCart < ActiveRecord::Migration
       end
     end
   end
+
+  def down
+    say "split items with quantity>2 into multiple items"
+    LineItem.where("quantity>1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id, product_id: line_item.product_id, quantity: 1
+      end
+
+      # remove original item
+      line_item.destroy
+    end
+  end
 end
