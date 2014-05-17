3b8f618899aadcdd1fe9ef54b1b97b300b8d9f3a
yellowstar <845059331@qq.com>
2014-05-17 10:39:35 +0800

    Change the error message associated with one of my validations

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ActiveRecord::Base
     message: 'must be aURL for GIF, JPG or PNG image.'
   }
 
-  validates :title, length: {minimum: 10}
+  validates :title, length: {minimum: 10, too_short: "should be at least 10 characters"}
 
   def self.latest
     Product.order(:updated_at).last
