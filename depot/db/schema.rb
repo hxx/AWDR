92f2dbad6fa9b16da4ec99d0050f492436a847d3
yellowstar <845059331@qq.com>
2014-05-17 08:31:13 +0800

    rake db:migrate

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516234229) do
+ActiveRecord::Schema.define(version: 20140516235853) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
