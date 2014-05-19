4691d4d9b1072de7c79956de90aaf565c49ce466
yellowstar <845059331@qq.com>
2014-05-19 21:58:15 +0800

    have access to the cart when we create an order

--- depot/app/controllers/orders_controller.rb
+++ depot/app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
