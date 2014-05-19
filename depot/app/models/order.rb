8614bb1f643263be6f949faa8a64ca75e862976a
yellowstar <845059331@qq.com>
2014-05-19 22:29:02 +0800

    add validates for Order model

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
