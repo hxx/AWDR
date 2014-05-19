13fd9cc57671d4cc2b5b18e81edd102e4d180c57
yellowstar <845059331@qq.com>
2014-05-19 22:06:50 +0800

    add test “require item in cart” in order controller test

--- depot/test/controllers/orders_controller_test.rb
+++ depot/test/controllers/orders_controller_test.rb
@@ -11,6 +11,12 @@ class OrdersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:orders)
   end
 
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
   test "should get new" do
     get :new
     assert_response :success
