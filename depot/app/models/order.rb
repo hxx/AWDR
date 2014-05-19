c0fe114a2a96154735ad56798a2cfc7bd8798e5f
yellowstar <845059331@qq.com>
2014-05-19 22:40:22 +0800

    order has_many line_items

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
