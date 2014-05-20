ff4743b74d5b3b597f5012e49c68a9211ed53fe2
yellowstar <845059331@qq.com>
2014-05-20 23:13:50 +0800

    update routes for admin

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get 'admin' => 'admin#index'  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   get 'sessions/new'
 
