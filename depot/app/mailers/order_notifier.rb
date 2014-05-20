0b757df52a20ed98a17b55b747dd3bc864e2fc13
yellowstar <845059331@qq.com>
2014-05-20 22:19:00 +0800

    fix the method errors for forgetting give it an argument

--- depot/app/mailers/order_notifier.rb
+++ depot/app/mailers/order_notifier.rb
@@ -6,7 +6,7 @@ class OrderNotifier < ActionMailer::Base
   #
   #   en.order_notifier.received.subject
   #
-  def received
+  def received(order)
     @order = order
 
     mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
@@ -17,7 +17,7 @@ class OrderNotifier < ActionMailer::Base
   #
   #   en.order_notifier.shipped.subject
   #
-  def shipped
+  def shipped(order)
     @order = order
 
     mail to: order.email, subject: 'Pragmatic Store Order Shipped'
