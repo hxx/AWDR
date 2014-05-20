904e5cbe6c85f6e670b21c800ea3f766cc175ac5
yellowstar <845059331@qq.com>
2014-05-20 23:11:28 +0800

    fix the noice errors

--- depot/app/controllers/users_controller.rb
+++ depot/app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: 'User #{@user.name} was successfully created.' }
+        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url, notice: 'User #{user.name} was successfully updated.' }
+        format.html { redirect_to user_url, notice: "User #{user.name} was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
