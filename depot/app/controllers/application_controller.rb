4c9f175dd876c3c1d0acfe263b8725672f9f17b2
yellowstar <845059331@qq.com>
2014-05-21 22:23:54 +0800

    extract the locale from the parameters and make it available to the application

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   protected
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def defualt_url_options
+      { locale: I18n.locale }
+    end
+
     def authorize
       unless User.find_by(id: session[:user_id])
         redirect_to login_url, notice: "Please log in"
