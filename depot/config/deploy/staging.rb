4912c5d87633319bb0b1455acb3a1d86c35a2d60
yellowstar <845059331@qq.com>
2014-05-23 21:55:42 +0800

    add staging deploy config

---
+++ depot/config/deploy/staging.rb
@@ -0,0 +1,11 @@
+set :app_server, 'play.huangxingxing.info'
+set :app_url, 'http://depot.huangxingxing.info'
+set :application, app_server
+role :web, app_server
+role :app, app_server
+role :db,  app_server, :primary => true
+set :deploy_to, "/var/www/AWRD"
+set :user, "deploy"
+set :rvm_ruby_string, 'ruby-2.0.0-p481'
+set :branch, "master"
+set :rails_env, "staging"
