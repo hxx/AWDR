f857ee84ea8044753214cd4cd0f7308f65f2a0bf
yellowstar <845059331@qq.com>
2014-05-27 20:07:57 +0800

    remove authorise before_action in application controller

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :set_i18n_locale_from_params
-  before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
