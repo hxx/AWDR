0d71e5f6ac8e6eb6bd36a4f5b2cfecb6c948b453
yellowstar <845059331@qq.com>
2014-05-28 22:58:56 +0800

    add user.is_admin? to ability.rb

--- depot/app/models/ability.rb
+++ depot/app/models/ability.rb
@@ -28,5 +28,12 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/bryanrite/cancancan/wiki/Defining-Abilities
+    user ||= User.new # guest user (not logged in)
+    if user.is_admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
+
   end
 end
